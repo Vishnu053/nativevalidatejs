0f33ad69ecbc4d66403766cb072b7971958c0787
vishnu053 <vishnunair053@gmail.com>
2022-03-27 20:56:09 +0530

    Changes to validateFunction

--- nativeValidate.js
+++ nativeValidate.js
@@ -1,5 +1,5 @@
-function listenForValidation(v) {
-    let children = document.querySelectorAll('.' + v + ' input');
+function listenForValidation(classToValidate) {
+    let children = document.querySelectorAll('.' + classToValidate + ' input');
     for (let i = 0; i < children.length; i++) {
         children[i].addEventListener('change', function () {
             validateSingleChild(this);
@@ -10,36 +10,47 @@ function listenForValidation(v) {
     }
 }
 
-function validateSingleChild(c){
-    let valid = c.checkValidity();
+function validateSingleChild(classToValidate){
+    let valid = classToValidate.checkValidity();
     if(valid){
-        c.classList.remove('is-invalid');
-        c.classList.add('is-valid');
-        let valErr = c.parentNode.querySelector('valerr');
+        classToValidate.classList.remove('is-invalid');
+        classToValidate.classList.add('is-valid');
+        let valErr = classToValidate.parentNode.querySelector('valerr');
         if(valErr){
             valErr.remove();
         }
     }else{
-        c.classList.remove('is-valid');
-        c.classList.add('is-invalid');
-        c.insertAdjacentHTML("afterend", `<valerr style='color:red'>${c.validationMessage}</valerr>`)
+        classToValidate.classList.remove('is-valid');
+        classToValidate.classList.add('is-invalid');
+        classToValidate.insertAdjacentHTML("afterend", `<valerr style='color:red'>${classToValidate.validationMessage}</valerr>`)
     }
 }
 
-function validateInputs(v) {
-    let inputs = document.querySelectorAll('.' + v + ' input');
-    if (inputs.length > 0) {
-        document.querySelectorAll("valerr").forEach(e=>e.remove())
-        for (let i of inputs) {
-            if (i.validationMessage) {
-                i.classList.add('is-invalid');
-                i.insertAdjacentHTML("afterend", `<valerr style='color:red'>${i.validationMessage}</valerr>`)
-            } else {
-                if (i.classList.toString().includes('is-invalid')) {
-                    i.classList.add("is-valid")
+function validateInputs(classToValidate) {
+    return new Promise((resolve,reject)=>{
+        let inputs = document.querySelectorAll('.' + classToValidate + ' input');
+        if (inputs.length > 0) {
+            document.querySelectorAll("valerr").forEach(e=>e.remove())
+            let errorCount=0
+            for (let i of inputs) {
+                if (i.validationMessage) {
+                    i.classList.add('is-invalid');
+                    i.insertAdjacentHTML("afterend", `<valerr style='color:red'>${i.validationMessage}</valerr>`)
+                    errorCount++
+                } else {
+                    if (i.classList.toString().includes('is-invalid')) {
+                        i.classList.add("is-valid")
+                    }
+                    i.classList.remove('is-invalid');
                 }
-                i.classList.remove('is-invalid');
             }
+            if(errorCount>0){
+                reject(errorCount)
+            }else{
+                resolve(true)
+            }
+        }else{
+            resolve(true);
         }
-    }
+    })
 }
